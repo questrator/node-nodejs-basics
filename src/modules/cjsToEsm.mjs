4281c5067d65b3309b2d51aaf197632df9d43aeb
questrator <drumtrack@ya.ru>
2022-06-05 20:00:53 +0300

    update

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,30 +1,24 @@
 import path from "path";
+import { fileURLToPath } from 'url';
 import { release, version } from "os";
 import { createServer as createServerHttp } from "http";
 import a from "./files/a.json" assert {type: "json"};
 import b from "./files/b.json" assert {type: "json"};
-
-//require('./files/c');
+import "./files/c.js";
 
 const random = Math.random();
 
-let unknownObject;
-
-random > 0.5 ? unknownObject = a : unknownObject = b;
+let unknownObject = random > 0.5 ? a : b;
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-// console.log(`Path to current file is ${__filename}`);
-// console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${fileURLToPath(import.meta.url)}`);
+console.log(`Path to current directory is ${path.dirname(fileURLToPath(import.meta.url))}`);
 
-// const createMyServer = createServerHttp((_, res) => {
-//     res.end('Request accepted');
-// });
+const createMyServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
 
-// module.exports = {
-//     unknownObject,
-//     createMyServer,
-// };
-
+export { unknownObject, createMyServer }
