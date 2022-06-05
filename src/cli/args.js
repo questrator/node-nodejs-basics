7247cc1887bbb0bad6d2f181b548a3451d7a8ab7
questrator <drumtrack@ya.ru>
2022-06-05 20:23:53 +0300

    update

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,8 +1,7 @@
 export const parseArgs = () => {
-    const props = process.argv.filter((e, i, a) => {
-        if (e.startsWith("-")) e += a[i + 1];
-    });
-    console.log(props);
+    const props = process.argv
+    .map((e, i, a) => e.startsWith("-") ? `${e.replace(/^-+/, "")} is ${a[i + 1]}` : null)
+    .forEach(e => e !== null ? console.log(e) : 0);
 };
 
 parseArgs();
